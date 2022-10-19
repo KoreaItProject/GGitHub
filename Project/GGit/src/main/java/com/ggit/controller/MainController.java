d806e9b233f21973d3ebf6bf0f1525122ac55fd9
TaeHyeon123 <harry7141@gmail.com>
2022-10-19 12:21:56 +0900

    123

--- Project/GGit/src/main/java/com/ggit/controller/MainController.java
+++ Project/GGit/src/main/java/com/ggit/controller/MainController.java
@@ -1,12 +1,14 @@
 package com.ggit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
-    @RequestMapping("/")
+    @GetMapping("/jj")
     public String hello() {
         return "/view/index.html";
     }
--- Project/GGit/vue/vue-front/src/router/index.js
+++ Project/GGit/vue/vue-front/src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/jj',
+      name: 'HelloWorldjj',
+      component: HelloWorldjj
     }
   ]
 })
