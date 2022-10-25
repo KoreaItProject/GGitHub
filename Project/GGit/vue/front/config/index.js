ce0c48d0a4c275697ca02b1fd16764afd0494902
TaeHyeon123 <harry7141@gmail.com>
2022-10-25 14:31:56 +0900

    gg

--- Project/GGit/vue/front/config/index.js
+++ Project/GGit/vue/front/config/index.js
@@ -13,9 +13,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 4520, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
