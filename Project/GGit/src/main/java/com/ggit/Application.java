ec1a51388ebbde6f8b2efe3c29f83e0c70a95557
TaeHyeon123 <harry7141@gmail.com>
2022-10-27 09:04:51 +0900

    11

--- Project/GGit/src/main/java/com/ggit/Application.java
+++ Project/GGit/src/main/java/com/ggit/Application.java
@@ -3,11 +3,14 @@ package com.ggit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ggit.socket.ServerMain;
+
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		// new ServerMain();
 
 	}
 
---
+++ Project/GGit/src/main/java/com/ggit/socket/ServerMain.java
@@ -0,0 +1,39 @@
+package com.ggit.socket;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.io.IOException;
+
+public class ServerMain {
+	private ServerSocket serverSocket;
+	private List<ServerHandler> list;
+	public static List<String> member;
+	public static HashMap<String, Integer> room;
+
+	public ServerMain() {
+		System.out.println("서버시작");
+		try {
+			serverSocket = new ServerSocket(9500);
+			serverSocket.setSoTimeout(0);
+			System.out.println("서버 준비 완료");
+			list = new ArrayList<ServerHandler>();
+			member = new ArrayList<>();
+			room = new HashMap<>();
+
+			while (true) {
+				System.out.println(123);
+				Socket socket = serverSocket.accept();
+				ServerHandler handler = new ServerHandler(socket, list); // 스레드를 생성한 것이랑 동일함! 떄문에 시자해주어야
+				handler.start(); // 스레드 시작- 스레드 실행
+				list.add(handler); // 핸들러를 담음( 이 리스트의 개수가 클라이언트의 갯수!!)
+
+			} // while
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
