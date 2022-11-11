4c53283840d46221e2afe345d71a388c0fc73af0
TaeHyeon123 <harry7141@gmail.com>
2022-11-11 16:15:56 +0900

    asdf

--- Project/front/src/components/repository/history.vue
+++ Project/front/src/components/repository/history.vue
@@ -1,10 +1,30 @@
 <template lang="">
-    <div>
-        <h1>history</h1>
+    <div class="repository_history_container">
+        <div class ="repository_history_div">
+            <div class="repository_history_left">
+                <table class="repository_history_table">
+                    <tr>
+                        <th width="50%">내용</th>
+                        <th>날짜</th>
+                        <th>사용자</th>
+                        <th>토큰</th>
+                    </tr>
+                    <tr>
+                        <td>내용</td>
+                        <td>내용</td>
+                        <td>내용</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="repository_history_right">
+
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang="">
+<style lang="sass">
+@import "src/assets/sass/repository/history"
 </style>
