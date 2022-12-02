294947b8415e19bcf7bbc1782563291462f71b1f
TaeHyeon123 <harry7141@gmail.com>
2022-12-02 12:22:29 +0900

    ㅁㅇㄴㄹ

--- DB/SQL/ggitdb_repomem.sql
+++ DB/SQL/ggitdb_repomem.sql
@@ -1,53 +1,133 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+
 --
+
 -- Host: localhost    Database: ggitdb
+
 -- ------------------------------------------------------
+
 -- Server version	8.0.31
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+/*!50503 SET NAMES utf8 */
+
+;
+
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+
+;
+
+/*!40103 SET TIME_ZONE='+00:00' */
+
+;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+
+;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+
+;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+
+;
+
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+
+;
 
 --
+
 -- Table structure for table `repomem`
+
 --
 
 DROP TABLE IF EXISTS `repomem`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `repomem` (
-  `idx` int NOT NULL AUTO_INCREMENT,
-  `repo` int NOT NULL COMMENT '저장소 번호\n',
-  `member` int NOT NULL COMMENT '회원번호',
-  `sort` int DEFAULT '0',
-  PRIMARY KEY (`idx`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+/*!50503 SET character_set_client = utf8mb4 */
+
+;
+
+CREATE TABLE
+    `repomem` (
+        `idx` int NOT NULL AUTO_INCREMENT,
+        `repo` int NOT NULL COMMENT '저장소 번호\n',
+        `member` int NOT NULL COMMENT '회원번호',
+        `sort` int DEFAULT '0',
+        PRIMARY KEY (`idx`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 20 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
 
 --
+
 -- Dumping data for table `repomem`
+
 --
 
 LOCK TABLES `repomem` WRITE;
-/*!40000 ALTER TABLE `repomem` DISABLE KEYS */;
-INSERT INTO `repomem` VALUES (1,1,1,0),(2,2,1,1),(3,3,8,0),(4,4,3,0),(5,5,1,2),(6,6,6,0),(7,7,6,1),(8,8,9,0),(9,9,8,1),(10,10,3,1),(11,1,4,0),(12,2,4,1),(13,3,7,0),(14,4,10,0),(15,11,4,2),(16,12,7,1),(17,12,1,3),(18,11,1,4),(19,2,2,0);
-/*!40000 ALTER TABLE `repomem` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40000 ALTER TABLE `repomem` DISABLE KEYS */
+
+;
+
+INSERT INTO `repomem`
+VALUES (1, 1, 1, 0), (2, 2, 1, 1), (3, 3, 8, 0), (4, 4, 3, 0), (5, 5, 1, 2), (6, 6, 6, 0), (7, 7, 6, 1), (8, 8, 9, 0), (9, 9, 8, 1), (10, 10, 3, 1), (11, 1, 4, 0), (12, 2, 4, 1), (13, 3, 7, 0), (14, 4, 10, 0), (15, 11, 4, 2), (16, 12, 7, 1), (17, 12, 1, 3), (18, 11, 1, 4), (19, 2, 2, 0);
+
+/*!40000 ALTER TABLE `repomem` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
+
+;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+
+;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+
+;
+
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+
+;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+
+;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+
+;
 
 -- Dump completed on 2022-12-02 12:20:53
