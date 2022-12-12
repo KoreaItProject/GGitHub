9ad913d040c4945461d836e2295fa8384b2f5f7b
김진렬 <>
2022-12-12 13:54:34 +0900

    병합 디자인 한거까지만 올림

--- Project/front/src/components/repository/history/mainHistory.vue
+++ Project/front/src/components/repository/history/mainHistory.vue
@@ -17,23 +17,23 @@
 
             </table>
             <div class="repository_history_table_data_div scrollBar">
-                <table class="repository_history_table" v-for="(data,index) in history">            
+              <table class="repository_history_table" v-for="(data,index) in history">            
                 <colgroup>
-                        <col width="65%">
-                        <col width="10%">
-                        <col width="15%">
-                        <col width="10%">
+                  <col width="65%">
+                  <col width="10%">
+                  <col width="15%">
+                  <col width="10%">
                 </colgroup>  
                 <tr @click="clickIndex=index">
-                    <td width="65%">메인 내용</td>
-                    <td>날짜</td>
-                    <td>사용자</td>
-                    <td>토큰</td>
+                  <td width="65%">메인 내용</td>
+                  <td>날짜</td>
+                  <td>사용자</td>
+                  <td>토큰</td>
                 </tr>
                 <div class="repository_history_click_container" v-show="clickIndex==index">
                     <button>내 작업 영역으로 가져오기</button>
                 </div>
-            </table>
+              </table>
             </div>
         </div>
     </div>
@@ -44,13 +44,13 @@ export default {
     return {
       clickIndex: -1,
       history: [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
         "",
         "",
         "",
---
+++ Project/front/src/components/repository/pullrequest_nomem.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div class="pullreq_nomem_main">
+        <div class="pullreq_nomem_header">
+            <div></div>
+        </div>
+        <div class="pullreq_nomem_list">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
