3ad00f241caf065d9836f94fcdbd51cbcc0c95e6
TaeHyeon123 <harry7141@gmail.com>
2022-12-13 16:31:13 +0900

    세상에...

--- Project/front/src/components/repository/history/history.vue
+++ Project/front/src/components/repository/history/history.vue
@@ -2,16 +2,24 @@
     <div class="repository_history_container">
         <div class ="repository_history_div">
             <div class="repository_history_left">
+              <div class="repository_history_head">
+                <div class="repository_history_tit"><h3>{{isMain?"메인":"작업"}} 히스토리</h3></div>
                   <select name="barnch_select" class="branch_select_btn" v-model="isMain">
                         <option :value=true selected>메인</option> 
-                        <option :value=false>내 작업영역</option> 
+                        <option :value=false>작업</option> 
                   </select>
+              </div>
+                
               <mainHistory v-if="isMain"/>
               <myHistory v-if="!isMain"/>
             </div>
-            <div class="repository_history_right">
+            <div class="repository_history_right" @click="false">
+              <h3 v-if="!isMain">메인 히스토리</h3>
+              <h3 v-if="isMain">작업 히스토리</h3>
               <div class="history_right">
-
+              <mainHistory v-if="!isMain"/>
+              
+              <myHistory v-if="isMain"/>
               </div>
             </div>
             
