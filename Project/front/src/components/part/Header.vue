bc7ab6fc0922bb778ad81049f07fd6a0b8035a56
TaeHyeon123 <harry7141@gmail.com>
2022-12-22 16:18:29 +0900

    header고정이랑 프로필 디자인 바뀜 근데 이전이 나은거 같아서 다시 돌렸는데 갑자기 이게 나은거 같아서 다시 돌렸는다 돌리고 나니까 뭐가 좋은지 모르겠는데 이게 더 나은것같기도

--- Project/front/src/components/part/Header.vue
+++ Project/front/src/components/part/Header.vue
@@ -1,32 +1,34 @@
 <template>
   <header>
-    <div class="header_container">
-      <a href="/" class="header_logo">
-        <img class="logo" src="@/assets/imgs/logo/logo.png" />
-        <span class="header_logo_text">GGit</span>
-      </a>
+    <div class="header_margin">
+      <div class="header_container">
+        <a href="/" class="header_logo">
+          <img class="logo" src="@/assets/imgs/logo/logo.png" />
+          <span class="header_logo_text">GGit</span>
+        </a>
 
-      <!-- 로그인 됐을때 -->
-      <div class="header_right_div" v-if="islogin">
-        <img :src="profileImg" class="header_profile_img" />
-        <div class="header_tab_div">
-          <a class="header_tab" @click="Logout"><span>로그아웃</span></a>
-          <a class="header_tab" href="/setting/profile"><span>설정</span></a>
-          <a class="header_tab" href="/download"><span>다운로드</span></a>
-          <a class="header_tab" href="/create"><span>새 저장소</span></a>
-          <a class="header_tab" :href="'/' + nick + '?tab=repositories'"
-            ><span>내 저장소</span></a
-          >
-          <a class="header_tab" :href="'/' + nick"><span>프로필</span></a>
+        <!-- 로그인 됐을때 -->
+        <div class="header_right_div" v-if="islogin">
+          <img :src="profileImg" class="header_profile_img" />
+          <div class="header_tab_div">
+            <a class="header_tab" @click="Logout"><span>로그아웃</span></a>
+            <a class="header_tab" href="/setting/profile"><span>설정</span></a>
+            <a class="header_tab" href="/download"><span>다운로드</span></a>
+            <a class="header_tab" href="/create"><span>새 저장소</span></a>
+            <a class="header_tab" :href="'/' + nick + '?tab=repositories'"
+              ><span>내 저장소</span></a
+            >
+            <a class="header_tab" :href="'/' + nick"><span>프로필</span></a>
+          </div>
         </div>
-      </div>
 
-      <!-- 로그인 안됐을 때 -->
-      <div class="header_right_div" v-if="!islogin">
-        <div class="header_tab_div">
-          <a class="header_tab"><span>회원가입</span></a>
-          <a class="header_tab" href="/login"><span>로그인</span></a>
-          <a class="header_tab" href="/download"><span>다운로드</span></a>
+        <!-- 로그인 안됐을 때 -->
+        <div class="header_right_div" v-if="!islogin">
+          <div class="header_tab_div">
+            <a class="header_tab"><span>회원가입</span></a>
+            <a class="header_tab" href="/login"><span>로그인</span></a>
+            <a class="header_tab" href="/download"><span>다운로드</span></a>
+          </div>
         </div>
       </div>
     </div>
