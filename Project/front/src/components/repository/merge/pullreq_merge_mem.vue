645e242425e0a9ef7abe6f5ef8d58b6cd57b1ff7
JIN-RYEOL <0205kjl@naver.com>
2023-01-07 20:03:40 +0900

    디자인 토나와

--- Project/front/src/components/repository/merge/pullreq_merge_mem.vue
+++ Project/front/src/components/repository/merge/pullreq_merge_mem.vue
@@ -3,7 +3,7 @@
         <div class="pullreq_merge_name">
 
         </div>
-        <div class="pullreq_merge_container">
+        <div class="pullreq_merge_container" :style="cssVariable">
             <div class="pullreq_merge_div_left">
                 <div class="pullreq_merge_div_left2">
                     <div class="pullreq_merge_div_left_data" v-for="(data, index) in test_data">
@@ -21,7 +21,6 @@
                     </div>
                 </div>
                 <div class="pullreq_merge_div_right_top" v-if="pullreq_merge_right_top_state == true" :style="cssVariable">
-                    <!-- <span>111</span> -->
                     <div class="pullreq_merge_div_right_top_left_contain">
                         <sapn>&nbsp;&nbsp;메인 파일</sapn>
                         <div class="pullreq_merge_div_right_top_left">
@@ -54,8 +53,11 @@
                 </div>
 
                 </br>
-                <span style="margin-top:300px;">&nbsp;&nbsp;병합 비교</span>
-                <div class="pullreq_merge_div_right_bottom">
+
+                <div class="pullreq_merge_div_right_bottom_span" :style="cssVariable">
+                    <span>&nbsp;&nbsp;병합 비교</span>
+                </div>
+                <div class="pullreq_merge_div_right_bottom" :style="cssVariable">
                     <div class="pullreq_merge_div_right_bottom_changecode">
                         <table>
                             <tr v-for="(data,index) in test_line">
@@ -82,14 +84,14 @@ export default {
             test_data:["","",""],
             test_line:[],
 
-            pullreq_merge_right_top_state: true,
+            pullreq_merge_right_top_state: false,
 
+            pullreq_merge_div_right_bottom_span: '0px',
+            test1: '1000px',
+            test2: '300px',
+            test3: '150%',
+            test4: '165%'
 
-            top: '',
-            position: '',
-            transition: '',
-            height: '',
-            back: ""
         }
     },
     mounted(){
@@ -106,23 +108,38 @@ export default {
             })
         },
         pullreq_merge_right_top_state_func(){
-            this.top = '0%';
-            this.position = 'relative';
-            this.transition = 'all .3s ease-out';
-            this.height = '100%';
-            this.back = "black";
             this.pullreq_merge_right_top_state = !this.pullreq_merge_right_top_state;
+            if(this.pullreq_merge_right_top_state){ // 폈을때
+                this.pullreq_merge_div_right_bottom_span = '280px';
+                this.test1 = "2000px";
+                this.test2 = "300px";
+                if(window.innerWidth < 1100){
+                   this.test3 = "70%"
+                }else{
+                    this.test3 = "70%" 
+                }
+            }else{ // 접혔을때
+                this.pullreq_merge_div_right_bottom_span = '0px';
+                if(window.innerWidth < 1100){
+                    this.test1 = "1200px";
+                }else{
+                    this.test1 = "1000px";
+                }
+                this.test1 = "1000px";
+                this.test2 = "300px";
+                this.test3 = "150%";
+            }
+           
         }
     },
     computed: {
         cssVariable(){
             return {
-                "--top": this.top,
-                "--position": this.position,
-                "--transition": this.transition,
-                "--background": this.background,
-                "--height": this.height,
-                "--back": this.back
+                "--pullreq_merge_div_right_bottom_span": this.pullreq_merge_div_right_bottom_span,
+                "--test1": this.test1,
+                "--test2": this.test2,
+                "--test3": this.test3,
+                "--test4": this.test4,
             }
         }
     }
