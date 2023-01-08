bc19f60177c3c3890649dfa9f0575ad9f4970c0f
JIN-RYEOL <0205kjl@naver.com>
2023-01-08 19:27:01 +0900

    병합 뒤로가기 버튼

--- Project/front/src/components/repository/merge/pullreq_merge_mem.vue
+++ Project/front/src/components/repository/merge/pullreq_merge_mem.vue
@@ -1,25 +1,56 @@
 <template lang="">
     <div>
         <div class="pullreq_merge_name">
-
+            
         </div>
         <div class="pullreq_merge_container" :style="cssVariable">
             <div class="pullreq_merge_div_left">
                 <div class="pullreq_merge_div_left2">
+                    <div class="pullreq_merge_div_left2_div">
+                        <button class="pullreq_merge_div_left2_backBtn" @click="merge_func_close()">
+                            <svg style="width:15px; height:15px;"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    
                     <div class="pullreq_merge_div_left_data" v-for="(data, index) in test_data">
-                        {{index}}
+                        <div class="left_data_top">
+                            <div class="left_data_top_filename">
+                                <span>fileTest.html</span>
+                            </div>
+                        </div>
+                        <div class="left_data_bottom">
+                            <div class="left_data_bottom_filepath">
+                                <span>C:/data/path/123</span>
+                            </div>
+                        </div>
                     </div>
                 </div> 
             </div>
+
             <div class="pullreq_merge_div_right">
-                <div class="test">
-                    <div v-if="pullreq_merge_right_top_state == true">
-                        <button class @click="pullreq_merge_right_top_state_func()">-</button>
-                    </div>
-                    <div v-if="pullreq_merge_right_top_state == false">
-                        <button @click="pullreq_merge_right_top_state_func()">+</button>
+                <div class="plus_minus_div">
+                    <div class="minus_div" v-if="pullreq_merge_right_top_state == true">
+                        <a class="minus_div_a" @click="pullreq_merge_right_top_state_func()">
+                            <div class="minus_div_a_div">
+                                <svg style="width:20px; height:20px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
+                                </svg>
+                            </div>
+                        </a>
+                    </div> 
+                    <div class="plus_div" v-if="pullreq_merge_right_top_state == false">
+                        <a class="plus_div_a" @click="pullreq_merge_right_top_state_func()">
+                            <div>
+                                <svg style="width:20px; height:20px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
+                                </svg>
+                            </div>
+                        </a>
                     </div>
                 </div>
+
                 <div class="pullreq_merge_div_right_top" v-if="pullreq_merge_right_top_state == true" :style="cssVariable">
                     <div class="pullreq_merge_div_right_top_left_contain">
                         <sapn>&nbsp;&nbsp;메인 파일</sapn>
@@ -64,7 +95,7 @@
                                 <td class="td1"></td>
                                 <td class="td2">{{index+1}}</td>
                                 <td class="td3">
-                                    <span class="td3_span" data-code-marker="+">{{data}}</span>
+                                    <span class="td3_span">{{data}}</span>
                                 </td>
                             </tr>
                         </table>
@@ -86,7 +117,7 @@ export default {
 
             pullreq_merge_right_top_state: false,
 
-            pullreq_merge_div_right_bottom_span: '0px',
+            pullreq_merge_div_right_bottom_span: '-20px',
             test1: '1000px',
             test2: '300px',
             test3: '150%',
@@ -95,10 +126,10 @@ export default {
         }
     },
     mounted(){
-        this.test();
+        this.getMergeFile();
     },
     methods: {
-        test(){
+        getMergeFile(){
             axios.post("/api/testcon", {
 
             })
@@ -119,7 +150,7 @@ export default {
                     this.test3 = "70%" 
                 }
             }else{ // 접혔을때
-                this.pullreq_merge_div_right_bottom_span = '0px';
+                this.pullreq_merge_div_right_bottom_span = '-20px';
                 if(window.innerWidth < 1100){
                     this.test1 = "1200px";
                 }else{
@@ -129,7 +160,9 @@ export default {
                 this.test2 = "300px";
                 this.test3 = "150%";
             }
-           
+        },
+        merge_func_close(){
+            this.$emit("merge_func_close");
         }
     },
     computed: {
